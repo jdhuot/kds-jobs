8a29993531b0541f5b171ee41fe33ce38fe5d917
Jeremy <jeremy@bodiometer.com>
2023-08-30 23:24:37 -0600

    update auth

--- auth.js
+++ auth.js
@@ -3,7 +3,7 @@ const {OAuth2Client} = require('google-auth-library');
 
 const CLIENT_ID = "263638085300-2ensh9g76i4n0hdi586s9fih9tgsaib2.apps.googleusercontent.com";
 const CLIENT_SECRET = "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";
-const REDIRECT_URI = "/final";
+const REDIRECT_URI = "https://kds-jobs.netlify.app/final";
 
 const oauth2Client = new OAuth2Client(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI);
 
